eae1511a39e017692ab48f438e2414d20fff72ff
Oliver Faircliff <faircliff.oliver@gmail.com>
2013-11-20 16:20:52 +0000

    Changed adding of items to group the items together.

--- layout.py
+++ layout.py
@@ -5,6 +5,7 @@ class Canvas(QtGui.QGraphicsView):
     """
         This widget the interactive canvas which the layout is created.
     """
+
     def __init__(self):
         super(Canvas, self).__init__()
 
@@ -13,17 +14,30 @@ class Canvas(QtGui.QGraphicsView):
 
     def update(self, item):
         """ This method allows you to add a Item 
-            (From the items module.) to the canvas. """
+            (From the items module.) to the canvas. 
+            It returns the group of Qitems 
+            that make up the item. """
+
         # First generate the vector information for that item.
         vectorItems = item.generateVectors()
+
         # Then for each vector, find the type and create the Qitems for it.
+        Qitems = []
         for item in vectorItems:
             if item[0] == 'line':
-                Qitem = QtCore.QLineF(float(item[1]['x1']),
+                Qitems.append(
+                    self.scene.addLine(
+                        QtCore.QLineF(float(item[1]['x1']),
                                       float(item[1]['y1']),
                                       float(item[1]['x2']),
                                       float(item[1]['y2']))
-            self.scene.addLine(Qitem)
+                    )
+                )
+        # Store all the Qitems in a group so they behave as one item.
+        group = self.scene.createItemGroup(Qitems)
+        group.setFlag(QtGui.QGraphicsItem.ItemIsMovable)
+        
+        return group
 
 def main():
     app = QtGui.QApplication(sys.argv)
