fcf4c1e6b13cb469d133d063416557369ffef68c
olls <olls@ovi.com>
2014-01-17 14:00:29 +0000

    Pritty much finished edit window, could do with some cleaning.

--- items/edit.py
+++ items/edit.py
@@ -16,21 +16,24 @@ class Edit(QtGui.QMainWindow):
             self.values = {
                 'length':   QtGui.QLineEdit(self),
                 'width':    QtGui.QLineEdit(self),
+                'height':   QtGui.QLineEdit(self),
+                'bow':   QtGui.QLineEdit(self),
+                'stern':   QtGui.QLineEdit(self),
                 'wallWidth':   QtGui.QLineEdit(self),
-                'height':   QtGui.QLineEdit(self)#,
-                # 'color':    QtGui.QPushButton('Color')
+                'author':   QtGui.QLineEdit(self),
+                'color':    ColorPicker()
             }
         elif 'Furniture' in str(self.item.__class__):
             self.values = {
                 'scale':   QtGui.QLineEdit(self),
-                'angle':    QtGui.QLineEdit(self),
-                'color':    QtGui.QPushButton('Color')
+                # 'angle':    QtGui.QLineEdit(self),
+                'color':    ColorPicker()
             }
         elif 'Wall' in str(self.item.__class__):
             self.values = {
                 'width':    QtGui.QLineEdit(self),
                 'door y':   QtGui.QLineEdit(self),
-                'color':    QtGui.QPushButton('Color')
+                'color':    ColorPicker()
             }
 
         self.initUI()
@@ -43,9 +46,11 @@ class Edit(QtGui.QMainWindow):
         y = 0
         for key, value in self.values.items():
             value.insert(str(self.item.attrs[key]))
-            self.grid.addWidget(QtGui.QLabel(Edit.capt(key)+':'), y, 0)
-            self.grid.addWidget(value, y+1, 0)
-            y += 2
+            if not key == 'color':
+                self.grid.addWidget(QtGui.QLabel(Edit.capt(key)+':'), y, 0)
+                y += 1
+            self.grid.addWidget(value, y, 0)
+            y += 1
 
         self.grid.addWidget(QtGui.QLabel('Description:'), 0, 1)
         self.description = QtGui.QTextEdit(self)
@@ -53,7 +58,7 @@ class Edit(QtGui.QMainWindow):
         self.grid.addWidget(self.description, 1, 1, y, 1)
 
         btn = QtGui.QPushButton('OK')
-        btn.clicked.connect(self.save)
+        btn.clicked.connect(self.ok)
         self.grid.addWidget(btn, y, 0)
 
         mainF.setLayout(self.grid)
@@ -61,17 +66,17 @@ class Edit(QtGui.QMainWindow):
 
         self.setWindowTitle('Editing '+ self.item.attrs['name'])
 
-    def save(self):
+    def ok(self):
         for key, value in self.values.items():
             v = value.text()
             try:
                 v = int(v)
-            except ValueError:
+            except (TypeError, ValueError):
                 try:
-                    v = str(v)
-                except ValueError:
+                    v = '"'+str(v)+'"'
+                except (TypeError, ValueError):
                     pass
-            command = 'self.item.set{key}({value})'.format(key = Edit.capt(key), value = value.text())
+            command = 'self.item.set{key}({value})'.format(key = Edit.capt(key), value = v)
             print(command)
             exec(command)
         print(self.description.toPlainText())
@@ -81,6 +86,30 @@ class Edit(QtGui.QMainWindow):
     def capt(string):
         """ Takes a string, and capitalizes the first letter. """
         return string[:1].upper() + string[1:]
+
+class ColorPicker(QtGui.QPushButton):
+    """ A button widget which activates a color
+        picker, and it is the color it is given. """
+    def __init__(self):
+        super(ColorPicker, self).__init__('Color')
+        
+        self.color = QtGui.QColor(0, 0, 0) 
+        self.clicked.connect(self.showDialog)
+
+
+    def update(self):
+        self.setStyleSheet('QPushButton { background-color: '+self.text()+'; color: ' + str('white' if self.color.lightness() < 127 else 'black') + '}')
+        
+    def showDialog(self):      
+        self.color = QtGui.QColorDialog.getColor(self.color)
+        self.update()
+
+    def insert(self, color):
+        self.color = QtGui.QColor(color)
+        self.update()
+
+    def text(self):
+        return self.color.name()
 
 def main():
     app = QtGui.QApplication(sys.argv)
