3898ec4951c7a6dee018c1c7d883231a96ad23ab
olls <olls@ovi.com>
2014-01-24 10:55:58 +0000

    Fixed bug with saving scale incorrectly.

--- items/templates.py
+++ items/templates.py
@@ -5,7 +5,7 @@
 TEMPLATES = {
     'furniture': {
         'chair': {
-            'XML': '<chair x="{x}" y="{y}" scale="{scale}*.3" angle="{angle}" description="{description}" color="{color}" />',
+            'XML': '<chair x="{x}" y="{y}" scale="{scale}" angle="{angle}" description="{description}" color="{color}" />',
             'SVG': '<line stroke="#000000" stroke-width="1" x1="{x}" y1="{y}" x2="{x}" y2="{y}+({scale}*.4)" />\
                     <line stroke="#000000" stroke-width="1" x1="{x}" y1="{y}" x2=" {x}+({scale}*.4) " y2="{y}" />\
                     <line stroke="#000000" stroke-width="1" x1="{x}" y1=" {y}+({scale}*.4) " x2=" {x}+({scale}*.4) " y2=" {y}+({scale}*.4) " />\
