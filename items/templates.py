a593cdc3d29319fca156ecee863981faff2b4da2
olls <olls@ovi.com>
2014-02-27 09:22:50 +0000

    Fixed saving and opening with new furnature.

--- items/templates.py
+++ items/templates.py
@@ -4,7 +4,7 @@
 
 TEMPLATES = {
     'furniture': {
-        'chair north': {
+        'chair-north': {
             'XML': '<chair-north x="{x}" y="{y}" scale="{scale}" angle="{angle}" description="{description}" color="{color}" />',
             'SVG': ('<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                    x2="{x}"                y2="{y}+({scale}*.4)"       />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                    x2="{x}+({scale}*.4)"   y2="{y}"                    />\n'
@@ -12,7 +12,7 @@ TEMPLATES = {
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}+({scale}*.4)"   y1="{y}"                    x2="{x}+({scale}*.4)"   y2="{y}+({scale}*.4)"       />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}+(({scale}*.4))*.8"  x2="{x}+({scale}*.4)"   y2="{y}+(({scale}*.4))*.8 " />\n')
         },
-        'chair south': {
+        'chair-south': {
             'XML': '<chair-south x="{x}" y="{y}" scale="{scale}" angle="{angle}" description="{description}" color="{color}" />',
             'SVG': ('<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                    x2="{x}"                y2="{y}+({scale}*.4)"       />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                    x2="{x}+({scale}*.4)"   y2="{y}"                    />\n'
@@ -20,7 +20,7 @@ TEMPLATES = {
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}+({scale}*.4)"   y1="{y}"                    x2="{x}+({scale}*.4)"   y2="{y}+({scale}*.4)"       />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}+(({scale}*.4))*.2"  x2="{x}+({scale}*.4)"   y2="{y}+(({scale}*.4))*.2 " />\n')
         },
-        'chair east': {
+        'chair-east': {
             'XML': '<chair-east x="{x}" y="{y}" scale="{scale}" angle="{angle}" description="{description}" color="{color}" />',
             'SVG': ('<line  stroke="{color}"    stroke-width="1"    x1="{x}"                    y1="{y}"                x2="{x}"                    y2="{y}+({scale}*.4)"   />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                    y1="{y}"                x2="{x}+({scale}*.4)"       y2="{y}"                />\n'
@@ -28,7 +28,7 @@ TEMPLATES = {
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}+({scale}*.4)"       y1="{y}"                x2="{x}+({scale}*.4)"       y2="{y}+({scale}*.4)"   />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}+(({scale}*.4))*.2"  y1="{y}"                x2="{x}+(({scale}*.4))*.2"  y2="{y}+({scale}*.4)"   />\n')
         },
-        'chair west': {
+        'chair-west': {
             'XML': '<chair-west x="{x}" y="{y}" scale="{scale}" angle="{angle}" description="{description}" color="{color}" />',
             'SVG': ('<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                    x2="{x}"                y2="{y}+({scale}*.4)"       />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                    x2="{x}+({scale}*.4)"   y2="{y}"                    />\n'
@@ -52,8 +52,8 @@ TEMPLATES = {
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}+{scale}"        y1="{y}+(0.9*{scale})"  x2="{x}+(0.9*{scale})"  y2="{y}+(0.9*{scale})"  />\n')
         },
 
-        'one segment sofa south': {
-            'XML': '<one-seg-sofa-south x="{x}" y="{y}" scale="{scale}" angle="{angle}" description="{description}" color="{color}" />',
+        'one-segment-sofa-south': {
+            'XML': '<one-segment-sofa-south x="{x}" y="{y}" scale="{scale}" angle="{angle}" description="{description}" color="{color}" />',
             'SVG': ('<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                x2="{x}+(0.5*{scale})"  y2="{y}"                />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}+(0.5*{scale})"  x2="{x}+(0.5*{scale})"  y2="{y}+(0.5*{scale})"  />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                x2="{x}"                y2="{y}+(0.5*{scale})"  />\n'
@@ -64,8 +64,8 @@ TEMPLATES = {
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}+(0.1*{scale})"  y1="{y}+(0.1*{scale})"  x2="{x}+(0.1*{scale})"  y2="{y}+(0.5*{scale})"  />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}+(0.4*{scale})"  y1="{y}+(0.1*{scale})"  x2="{x}+(0.4*{scale})"  y2="{y}+(0.5*{scale})"  />\n')
         },
-        'two segment sofa south': {
-            'XML': '<two-seg-sofa-south x="{x}" y="{y}" scale="{scale}" angle="{angle}" description="{description}" color="{color}" />',
+        'two-segment-sofa-south': {
+            'XML': '<two-segment-sofa-south x="{x}" y="{y}" scale="{scale}" angle="{angle}" description="{description}" color="{color}" />',
             'SVG': ('<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                x2="{x}+{scale}"        y2="{y}"                />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}+(0.5*{scale})"  x2="{x}+{scale}"        y2="{y}+(0.5*{scale})"  />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                x2="{x}"                y2="{y}+(0.5*{scale})"  />\n'
@@ -78,8 +78,8 @@ TEMPLATES = {
                     # Segments
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}+(0.5*{scale})"  y1="{y}+(0.1*{scale})"  x2="{x}+(0.5*{scale})"  y2="{y}+(0.5*{scale})"  />\n')
         },
-        'three segment sofa south': {
-            'XML': '<three-seg-sofa-south x="{x}" y="{y}" scale="{scale}" angle="{angle}" description="{description}" color="{color}" />',
+        'three-segment-sofa-south': {
+            'XML': '<three-segment-sofa-south x="{x}" y="{y}" scale="{scale}" angle="{angle}" description="{description}" color="{color}" />',
             'SVG': ('<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                x2="{x}+(1.5*{scale})"  y2="{y}"                />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}+(0.5*{scale})"  x2="{x}+(1.5*{scale})"  y2="{y}+(0.5*{scale})"  />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                x2="{x}"                y2="{y}+(0.5*{scale})"  />\n'
@@ -94,8 +94,8 @@ TEMPLATES = {
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}+{scale}"        y1="{y}+(0.1*{scale})"  x2="{x}+{scale}"        y2="{y}+(0.5*{scale})"  />\n')
         },
 
-        'one segment sofa north': {
-            'XML': '<one-seg-sofa-north x="{x}" y="{y}" scale="{scale}" angle="{angle}" description="{description}" color="{color}" />',
+        'one-segment-sofa-north': {
+            'XML': '<one-segment-sofa-north x="{x}" y="{y}" scale="{scale}" angle="{angle}" description="{description}" color="{color}" />',
             'SVG': ('<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                x2="{x}+(0.5*{scale})"  y2="{y}"                />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}+(0.5*{scale})"  x2="{x}+(0.5*{scale})"  y2="{y}+(0.5*{scale})"  />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                x2="{x}"                y2="{y}+(0.5*{scale})"  />\n'
@@ -106,8 +106,8 @@ TEMPLATES = {
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}+(0.1*{scale})"  y1="{y}"                x2="{x}+(0.1*{scale})"  y2="{y}+(0.4*{scale})"  />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}+(0.4*{scale})"  y1="{y}"                x2="{x}+(0.4*{scale})"  y2="{y}+(0.4*{scale})"  />\n')
         },
-        'two segment sofa north': {
-            'XML': '<two-seg-sofa-north x="{x}" y="{y}" scale="{scale}" angle="{angle}" description="{description}" color="{color}" />',
+        'two-segment-sofa-north': {
+            'XML': '<two-segment-sofa-north x="{x}" y="{y}" scale="{scale}" angle="{angle}" description="{description}" color="{color}" />',
             'SVG': ('<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                x2="{x}+{scale}"        y2="{y}"                />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}+(0.5*{scale})"  x2="{x}+{scale}"        y2="{y}+(0.5*{scale})"  />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                x2="{x}"                y2="{y}+(0.5*{scale})"  />\n'
@@ -120,8 +120,8 @@ TEMPLATES = {
                     # Segments
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}+(0.5*{scale})"  y1="{y}"                x2="{x}+(0.5*{scale})"  y2="{y}+(0.4*{scale})"  />\n')
         },
-        'three segment sofa north': {
-            'XML': '<three-seg-sofa-north x="{x}" y="{y}" scale="{scale}" angle="{angle}" description="{description}" color="{color}" />',
+        'three-segment-sofa-north': {
+            'XML': '<three-segment-sofa-north x="{x}" y="{y}" scale="{scale}" angle="{angle}" description="{description}" color="{color}" />',
             'SVG': ('<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                x2="{x}+(1.5*{scale})"  y2="{y}"                />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}+(0.5*{scale})"  x2="{x}+(1.5*{scale})"  y2="{y}+(0.5*{scale})"  />\n'
                     '<line  stroke="{color}"    stroke-width="1"    x1="{x}"                y1="{y}"                x2="{x}"                y2="{y}+(0.5*{scale})"  />\n'
